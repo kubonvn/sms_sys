ebc53ec5d034c88d40e10b50cc3ff4f9d4e3348f
SonND <sonnguyencreater@gmail.com>
2022-05-18 21:57:18 +0700

    update UserBusiness

--- src/main/java/com/examonline/business/UserBusiness.java
+++ src/main/java/com/examonline/business/UserBusiness.java
@@ -4,7 +4,7 @@ import com.examonline.database.dto.UserDTO;
 import com.examonline.exception.LogicException;
 import com.examonline.service.response.BaseResponse;
 import com.examonline.service.response.UserResponse;
-
+//hello anh em im wibulord
 public interface UserBusiness {
     //create user
     BaseResponse<UserResponse> registerUser(UserDTO user, String language) throws LogicException;
